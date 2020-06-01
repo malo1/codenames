0448f5d3c4f3d3ccb8d8afa612c1e3e2a75f6b3b
Marius Lorenz <mariuslorenz@hotmail.de>
2020-06-01 15:17:53 +0200

    added

--- codenames/board.py
+++ codenames/board.py
@@ -1,3 +1,25 @@
+"""
+This file contains our game board.
+It contains 5x5 cards for two teams.
+They contain the assassin, agents or none.
+"""
+TEAM_RED = 'red'
+TEAM_BLUE = 'blue'
+NO_TEAM
+
+
+class Card():
+    def __init__(self, team, word):
+        self.team = team
+        self.word = word
+
+
+
+class Board():
+    def __init__(self, cardlist, team):
+        self.wordlist = wordlist
+        self.team = team
+
 def greet(name):
 
     return f"Oh, Hi {name}"
